30f797a038ea485c1178acd9cc62522e40d8b835
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-08-13 12:20:55 +0900

    두 번째 다시 커밋

--- data_co_kr.py
+++
@@ -1,9 +0,0 @@
-import requests
-import xmltodict
-
-key="e690d7da713b49ec94a12b231edfe528"
-url="https://openapi.gg.go.kr/Tbggibllbrm={}".format(key)
-
-content=requests.get(url).content
-dic=xmltodict.parse(content)
-print(dic)
