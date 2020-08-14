1f6fec463026a0e48ce4db184a8811998498e068
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-08-14 17:10:17 +0900

    네 번째 커밋

--- header.php
+++ header.php
@@ -22,49 +22,50 @@
         if(!$userid1){
             ?>
             <div class="container">
-                <h1 class="title"><a href="index.php">Let' study</a></h1>
+                <div class="title"><a href="index.php"><img src="img/logo.png"></a></div>
                 <div class="nav">
                     <a href="login.php">
-                        <h4 class="todo">할일</h3>
+                        <h4 class="todo">할일</h4>
                     </a>
                     <a href="login.php">
-                        <h4 class="board">게시판</h3>
+                        <h4 class="board">게시판</h4>
                     </a>
                     <a href="login.php">
-                        <h4 class="question">Q & A</h3>
+                        <h4 class="question">Q & A</h4>
                     </a>
                     <a href="login.php">
-                        <h4 class="map">공부 지도</h3>
+                        <h4 class="map">공부 지도</h4>
                     </a>
                 </div>
                 <div class="member">
-                    <li><button onclick="location.href='login.php'">로그인</button></li>
-                    <li>||</li>
-                    <li><button onclick="location.href='agree.php'">회원가입</button></li>
+                    <a href="login.php"><h4 class="m_text">로그인</h4></a>
+                    <h4 class="m_text">|</h4>
+                    <a href="agree.php"><h4 class="m_text">회원가입</h4></a>
                 </div>
             </div>
         <?php
         }else{
         ?>
             <div class="container">
-                <h1 class="title"><a href="index.php">Let' study</a></h1>
+                <div class="title"><a href="index.php"><img src="img/logo.png"></a></div>
                 <div class="nav">
                     <a href="todo.php">
-                        <h4 class="todo">할일</h3>
+                        <h4 class="todo">할일</h4>
                     </a>
                     <a href="board.php">
-                        <h4 class="board">게시판</h3>
+                        <h4 class="board">게시판</h4>
                     </a>
                     <a href="question.php">
-                        <h4 class="question">Q & A</h3>
+                        <h4 class="question">Q & A</h4>
                     </a>
                     <a href="map.html">
-                        <h4 class="map">공부 지도</h3>
+                        <h4 class="map">공부 지도</h4>
                     </a>
                 </div>
                 <div class="member">
-                    <li><a href="modify.php"><h4>마이페이지</h4></a></li>
-                    <li><a href="logout.php"><h4>로그아웃</h4></a></li>
+                    <a href="modify.php"><h4 class="m_text">마이페이지</h4></a>
+                    <a href="#"><h4 class="m_text">|</h4></a>
+                    <a href="logout.php"><h4 class="m_text">로그아웃</h4></a>
                 </div>
             </div>
         <?php
