553c529d28aec37fc641e4b79bab4293605fb427
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-04 00:16:42 +0900

    todo delete ~~

--- php/todo_process.php
+++ php/todo_process.php
@@ -4,25 +4,15 @@
     else $u_idx="";
 
     // $conn=mysqli_connect('localhost', 'root', '111111', 'study');
-    $pdo = new PDO("mysql:host=localhost;dbname=study;charset=utf8","root","");
+    $pdo = new PDO("mysql:host=localhost;dbname=study;charset=utf8","root","111111");
     
     if(isset($_POST['submit'])) {
         $content = $_POST['content'];
-        $sql = "INSERT INTO todo (content, user_idx) VALUES ('$content', '$u_idx')";
-        if($conn -> query($sql) === TRUE) {
-            // header("Location : todo.php");
-        }else {
-            echo "Error : ". $sql . "<br>" . $conn -> error;
-        }
-
-        $conn -> close();
+        $sql = $pdo -> prepare("INSERT INTO todo (content, user_idx) VALUES ('$content', '$u_idx')");
+        $sql -> execute();
     } else if(isset($_POST['delete'])) {
         $id = $_POST['id'];
-        $sql = "DELETE FROM todo WHERE idx = '$id'";
-        if($conn -> query($sql) === TRUE) {
-            
-        }else {
-            echo "Error : ". $sql . "<br>" . $conn -> error;
-        }
+        $sql = $pdo -> prepare("DELETE FROM todo WHERE idx = '$id'");
+        $sql -> execute();
     }
 ?>
