01a734f6ddb184a4ef7a98fcbff809323daa8410
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-04 23:39:57 +0900

    Adding image upload

--- example.sql
+++ example.sql
@@ -11,3 +11,12 @@ INSERT INTO `todo` VALUES (1, 'hi', 16);
 INSERT INTO `todo` VALUES (2, 'h2i', 16);
 
 ALTER TABLE `board` ADD `user_idx` int(40) DEFAULT NULL;
+
+CREATE TABLE `images` (
+  `idx` int(40) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(100) NOT NULL,
+  `imgurl` varchar(512) NOT NULL,
+  `size` int NOT NULL,
+  `board_id` int(40),
+  PRIMARY KEY (`idx`)
+);
--- php/create_process.php
+++ php/create_process.php
@@ -1,28 +1,43 @@
 <?php
-    //echo $_FILES['file']
-    $conn=mysqli_connect('localhost', 'root', '111111', 'study');
-    $filtered=array(
-        'title'=>mysqli_real_escape_string($conn, $_POST['title']),
-        'description'=>mysqli_real_escape_string($conn, $_POST['description'])
-    );
+    $target_dir = "../uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-
-
-    $sql="INSERT INTO board
-            (name, title, description, created)
-            VALUES(
-                '{$_GET['name']}',
-                '{$_POST['title']}',
-                '{$_POST['description']}',
-                NOW()
-            )
-    ";
-    $result=mysqli_query($conn, $sql);
-    if($result===false){
-        echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.';
-        echo mysqli_error($conn);
-    }else{
-        Header("Location:upload.php"); 
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false){
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
     }
+
+    // $conn=mysqli_connect('localhost', 'root', '111111', 'study');
+    // $filtered=array(
+    //     'title'=>mysqli_real_escape_string($conn, $_POST['title']),
+    //     'description'=>mysqli_real_escape_string($conn, $_POST['description'])
+    // );
+
+
+
+    // $sql="INSERT INTO board
+    //         (name, title, description, created)
+    //         VALUES(
+    //             '{$_GET['name']}',
+    //             '{$_POST['title']}',
+    //             '{$_POST['description']}',
+    //             NOW()
+    //         )
+    // ";
+    // $result=mysqli_query($conn, $sql);
+    // if($result===false){
+    //     echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.';
+    //     echo mysqli_error($conn);
+    // }else{
+    //     Header("Location:upload.php"); 
+    // }
 
 ?>
