aa9ddd7470b223ea6d66ce5c9007e2fee3864732
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-05 02:40:35 +0900

    Adding board search~

--- php/todo_process.php
+++ php/todo_process.php
@@ -3,7 +3,7 @@
     if(isset($_SESSION["useridx"]))  $u_idx=$_SESSION["useridx"];
     else $u_idx="";
 
-    // $pdo = new PDO("mysql:host=localhost;dbname=study;charset=utf8","root","111111");
+    $pdo = new PDO("mysql:host=localhost;dbname=study;charset=utf8","root","111111");
     
     
     if(isset($_POST['submit'])) {
