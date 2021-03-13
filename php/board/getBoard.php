44f8296ebb2180930f320b88bd6dda52883d0c92
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-13 23:21:26 +0900

    search~

--- php/board/getBoard.php
+++ php/board/getBoard.php
@@ -7,10 +7,10 @@
 
     $sql = "SELECT * FROM board";
 
-    if($search != '') {
+    if(!empty($search)) {
         $sql."WHERE title LIKE '%".$search."%'";
     }
-
+   
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
