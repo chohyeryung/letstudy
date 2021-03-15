bc54337b2ed2b2b264d668766d2c3c69ec5d2cc4
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-15 23:24:30 +0900

    board_detail

--- php/board/board_detail_view.php
+++ php/board/board_detail_view.php
@@ -28,21 +28,23 @@
                         <div class="written_board">
                             <h1 class="board_title"><?=$row['title']?><h4 class="time"><?=$row['uploaded_on']?></h4></h1>
                             <hr>
-                            <img src="<?php echo $imageURL; ?>" alt=""/>
+                            <img src="<?php echo $imageURL; ?>" class="board-img" alt=""/>
                             <h3 class="des"><?=$row['description']?></h3>
-                            <a href="board_view.php"><button class="go_board">목록</button></a>
                         <?php
                             if($useridx==$row['uid']){
                         ?>
-                            <form action="board.php" method="POST">
-                                <input type="hidden" name="id" value="<?=$row['id']?>">
-                                <input type="submit" class="btn-delete" name="delete" value="삭제">
-                                <!-- <input type="submit" class="btn-update" name="update" value="수정"> -->
-                            </form>
-                            <form action="board.php" method="POST">
-                                <input type="hidden" name="id" value="<?=$row['id']?>">
-                                <input type="submit" class="btn-update" value="수정">
-                            </form>
+                            <div class="btn-con">
+                                <form action="board.php" method="POST">
+                                    <input type="hidden" name="id" value="<?=$row['id']?>">
+                                    <input type="submit" class="btn-update" value="수정">
+                                </form>
+                                <form action="board.php" method="POST">
+                                    <input type="hidden" name="id" value="<?=$row['id']?>">
+                                    <input type="submit" class="btn-delete" name="delete" value="삭제">
+                                    <!-- <input type="submit" class="btn-update" name="update" value="수정"> -->
+                                </form>
+                                <a href="board_view.php"><button class="go_board">목록</button></a>
+                            </div>
                         </div>
                         <?php
                             }
