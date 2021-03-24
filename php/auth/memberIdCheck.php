93b5cdfbb55b27242c148073bc36655df354da9d
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-24 22:42:28 +0900

    database code level up

--- php/auth/memberIdCheck.php
+++ php/auth/memberIdCheck.php
@@ -1,20 +1,14 @@
 <?php
-    //$pdo = new PDO("mysql:host=localhost;dbname=study;charset=utf8","root","111111");
     include '../../dbConfig.php';
 
     $memberId = $_POST['memberId'];
  
-    // $sql = "SELECT * FROM `member` WHERE id = '{$memberId}'";
     $sql = $db -> query("SELECT * FROM `member` WHERE id='$memberId'");
 
-    // $result = mysqli_query($conn, $sql);
-    // $result = mysqli_query($conn, $sql);
     if($sql -> num_rows >0) {
         $json = json_encode(array('res'=>'bad'));
         echo $json;
     }else {
         echo json_encode(array('res'=>'good'));
     }
-    
-    // $conn->close();
 ?>
