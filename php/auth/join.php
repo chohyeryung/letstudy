1636ca30931c41f93cbb5432f117fa3657205140
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-03-24 22:36:24 +0900

    database code level up

--- php/auth/join.php
+++ php/auth/join.php
@@ -1,23 +1,16 @@
 <?php
-    
-	// $conn=mysqli_connect('localhost', 'root', '111111', 'study') or die('fail');
     include '../../dbConfig.php';
 
     $userid = $_POST['memberId'];
-    // $userpw=hex(aes_encrypt($_POST['pw'],'userpw'));
     $userpw=password_hash($_POST['memberPw'], PASSWORD_DEFAULT);
 	$nickname=$_POST['memberNickName'];
     $birthday=$_POST['memberBirthDay'];
-	// $email= $_POST['email'].'@'.$_POST['emadress'];
     $email = $_POST['memberEmailAddress'];
  
+    //입력받은 데이터를 DB에 저장
     $sql = $db -> query("INSERT INTO `member` 
     (`id`, `pw`, `nickname`, `birthday`, `email`, `level`, `point`) 
     VALUES ('$userid', '$userpw', '$nickname', '$birthday', '$email', 9, 0)");
-
-
-    //입력받은 데이터를 DB에 저장
-    //$result = mysqli_query($conn, $query);
 
     //저장이 됬다면 (result = true) 가입 완료
     if($sql) {
--- php/auth/login.php
+++ php/auth/login.php
@@ -1,14 +1,16 @@
 <?php
  session_start();
- $conn=mysqli_connect('localhost', 'root', '111111', 'study');
+ include '../../dbConfig.php';
+//  $conn=mysqli_connect('localhost', 'root', '111111', 'study');
  //입력 받은 id와 password
  $id=$_POST['login_id'];
  $pw=$_POST['login_pw'];
  //아이디가 있는지 검사
- $sql="SELECT * FROM `member` WHERE id='$id'";
- $result=mysqli_query($conn, $sql);
- $num_match=mysqli_num_rows($result);
- if(!$num_match){
+ $sql = $db -> query("SELECT * FROM `member` WHERE id='$id'");
+//  $sql="SELECT * FROM `member` WHERE id='$id'";
+//  $result=mysqli_query($conn, $sql);
+//  $num_match=mysqli_num_rows($result);
+ if($sql -> num_rows <=0){
     echo("
         <script>
             window.alert('등록되지 않은 아이디입니다');
@@ -16,33 +18,30 @@
         </script>
     ");
 }else{
-    $row=mysqli_fetch_array($result);
-    mysqli_close($conn);
-    // echo $row['id'];
-    if((password_verify( $pw, $row['pw'] ))){   //$pass!=$db_pass
+    while($row = $sql -> fetch_assoc()){
+        if((password_verify( $pw, $row['pw'] ))){   //$pass!=$db_pass
         
-        $_SESSION["useridx"]=$row["idx"];
-        $_SESSION["userid"]=$row["id"];
-        $_SESSION["username"]=$row["nickname"];
-        $_SESSION["userlevel"]=$row["level"];
-        $_SESSION["userpoint"]=$row["point"];
-       
-        
-        
-        echo("
-            <script>
-                alert('로그인 되었습니다')
-                location.href='../index.php'
-            </script>
-        ");
-    }else{
-        echo("
-            <script>
-                alert('비밀번호가 틀립니다');
-                location.href='login_view.php';
-            </script>
-        ");
-        exit;
+            $_SESSION["useridx"]=$row["idx"];
+            $_SESSION["userid"]=$row["id"];
+            $_SESSION["username"]=$row["nickname"];
+            $_SESSION["userlevel"]=$row["level"];
+            $_SESSION["userpoint"]=$row["point"];
+           
+            echo("
+                <script>
+                    alert('로그인 되었습니다')
+                    location.href='../index.php'
+                </script>
+            ");
+        }else{
+            echo("
+                <script>
+                    alert('비밀번호가 틀립니다');
+                    location.href='login_view.php';
+                </script>
+            ");
+            exit;
+        }
     }
 }
 ?>
